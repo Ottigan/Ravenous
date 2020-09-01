7ab56cccdf11c4337707dcdfc25dee034aa79a99
Ottigan <64630527+Ottigan@users.noreply.github.com>
2020-09-01 15:24:19 +0200

    Adding skipped files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import logo from '../../logo.svg';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import BusinessList from '../BusinessList/BusinessList';
-import { render } from '@testing-library/react';
 
 class App extends React.Component {
 	render() {
